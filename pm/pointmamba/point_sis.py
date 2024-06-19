01caf8cdcf7657650ea15ee721d9a14de44ed365
archieren <renjianpeng@gmail.com>
2024-06-19 13:27:23 +0800

    kkk

--- pm/pointmamba/point_sis.py
+++ pm/pointmamba/point_sis.py
@@ -20,6 +20,10 @@ from mamba_ssm.ops.triton.layer_norm import RMSNorm, layer_norm_fn, rms_norm_fn
 from knn_cuda import KNN
 
 """
-用Mamba来处理点云,有下面的几样工作
+用Mamba来处理点云,有下面的几项工作:
+PointMamba:这哥们有点灌水,到第四版,又惨考了PTV3的结构化思路.
+Mamba3D:
+Point Cloud Mamba:
+另外Point Transformer V3的工作值得注意!尺度规模和结构化,是他想解决的问题!
 """
 
